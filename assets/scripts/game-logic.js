bb3cdbdf0c011ddd0dbdee70e1a9a56d51cb5321
taesch124 <taesch124@gmail.com>
2018-10-02 22:44:42 -0400

    Made it look better and made it so character starts at original damage if picked again.

--- assets/scripts/game-logic.js
+++ assets/scripts/game-logic.js
@@ -2,10 +2,15 @@ let characterSelected = false;
 let enemySelected = false;
 let gameOver = false;
 let damageIncrease;
+var characters;
 
 let player, enemy;
 
 $(document).ready (function() {
+    characters = [arthas, jaina, thrall, illidan];
+    for(let i = 0; i < characters.length; i++) {
+        generateCharacterCard(characters[i]).appendTo('#character-selection');
+    }
 
     $('.character-card').on('click', selectCharacter);
     $('.attack-button').on('click', attackEnemy);
@@ -22,14 +27,15 @@ function selectCharacter() {
     let counterAttack = characterElement.data('counter-attack');
     
     if(!characterSelected) {
-        player = new Character(name, health, attack, counterAttack);
+        player = findCharacterFromArray(characters, characterElement.attr('id'));
+        player.attackDamage = player.startingAttackDamage;
         damageIncrease = player.attackDamage;
         characterElement.addClass('player');
         characterElement.appendTo($('#your-character'));
         $('#character-selection .character-card').appendTo($('#available-enemies'));
         characterSelected = true;
     } else if(!enemySelected && !characterElement.hasClass('player')) {
-        enemy = new Character(name, health, attack, counterAttack);
+        enemy = findCharacterFromArray(characters, characterElement.attr('id'));
         characterElement.addClass('opponent');
         characterElement.appendTo('#attacking-enemy');
         enemySelected = true;
@@ -76,8 +82,7 @@ function enemyCounterAttack() {
 }
 
 function playerDied() {
-    printMessage('Game Over!');
-    printMessage('Select a new character.');
+    printEndGameMessage('Game Over!');
     switchAttackButton(true);
 }
 
@@ -106,7 +111,7 @@ function switchAttackButton(gameOver) {
 
 function checkWinCondition() {
     if($('#available-enemies .character-card').length === 0) {
-        printMessage("You win!");
+        printEndGameMessage("You win!");
         switchAttackButton(true);
     }
 }
@@ -115,18 +120,27 @@ function resetGame() {
     characterSelected = false;
     enemySelected = false;
     switchAttackButton(false);
+    $('#combat-log').empty();
     $('#your-character .character-card').removeClass('player');
     $('#your-character .character-card').appendTo('#character-selection');
     $('#attacking-enemy .character-card').removeClass('opponent');
     $('#attacking-enemy .character-card').appendTo('#character-selection');
     $('#defeated-enemies .character-card').appendTo('#character-selection');
     $('#available-enemies .character-card').appendTo('#character-selection');
-    //find a way to reset character health
-    let characters = $('#character-selection .character-card')
-    characters.each((i, e) => {
-        jQuery(characters[i]).children('.character-hitpoints').text(jQuery(characters[i]).data('health'));
+    //TODO - find a way to reset character health
+    let characterCards = $('#character-selection .character-card')
+    characterCards.each((i, e) => {
+        let current = findCharacterFromArray(characters, jQuery(characterCards[i]).attr('id'));
+        current.health = current.startingHealth;
+        jQuery(characterCards[i]).children('.character-hitpoints').text(current.startingHealth);
     });
         
+}
+
+function printEndGameMessage(message) {
+    let logString = $('<p id="end-game">');
+    logString.text(message);
+    logString.appendTo($('#combat-log'));
 }
 
 function printMessage(message) {
@@ -147,3 +161,85 @@ function Character(name, health, attack, counterAttack) {
     this.isDead = false;
 }
 
+function findCharacterFromArray(characters, id) {
+    return characters.find(x => x.id === id);
+}
+
+function generateCharacterCard(character) {
+    let card = $('<div>');
+    card.attr('id', character.id);
+    card.addClass('character-card');
+    if(character.horde) card.addClass('horde');
+    else card.addClass('alliance');
+
+    let name = $('<h3>');
+    name.addClass('character-name');
+    name.text(character.name);
+
+    let portrait = $('<img>');
+    portrait.addClass('character-portrait');
+    portrait.attr('src', character.src);
+    portrait.attr('alt', character.name + '\'s character portrait.');
+
+    let health = $('<h4>');
+    health.addClass('character-hitpoints');
+    health.text(character.health);
+
+    name.appendTo(card);
+    portrait.appendTo(card);
+    health.appendTo(card);
+
+    return card;
+}
+
+var arthas = {
+    id: 'arthas',
+    name: "Arthas Menethil",
+    startingHealth: 200,
+    health: 200,
+    startingAttackDamage: 6,
+    attackDamage: 6,
+    counterAttack: 12,
+    isDead: false,
+    src: './assets/images/characters/arthas-portrait.jpg',
+    horde: true
+}
+
+var jaina = {
+    id: 'jaina',
+    name: "Jaina Proudmoore",
+    startingHealth: 140,
+    health: 140,
+    startingAttackDamage: 9,
+    attackDamage: 9,
+    counterAttack: 25,
+    isDead: false,
+    src: './assets/images/characters/jaina-proudmoore-portrait.jpg',
+    horde: false
+}
+
+var thrall = {
+    id: 'thrall',
+    name: "Thrall",
+    startingHealth: 150,
+    health: 150,
+    startingAttackDamage: 8,
+    attackDamage: 8,
+    counterAttack: 21,
+    isDead: false,
+    src: './assets/images/characters/thrall-portrait.jpg',
+    horde: true
+}
+
+var illidan = {
+    id: 'illidan',
+    name: "Illidan Stormrage",
+    startingHealth: 160,
+    health: 160,
+    startingAttackDamage: 7,
+    attackDamage: 7,
+    counterAttack: 16,
+    isDead: false,
+    src: './assets/images/characters/illidan-portrait.jpg',
+    horde: false
+}
